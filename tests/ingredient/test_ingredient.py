29812e38c7aeacf9fa30f44fb3d7c1c871bb92fa
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-11-10 05:16:09 -0300

    req 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
 # Req 1
 def test_ingredient():
-    pass
+    # Testa se a classe Ingredient pode ser instanciada corretamente
+    ingredient = Ingredient("Cenoura")
+    assert ingredient.name == "Cenoura"
+    assert ingredient.restrictions == set()
+
+    # Testa se o método mágico __repr__ funciona como esperado
+    ingredient1 = Ingredient("Cenoura")
+    ingredient2 = Ingredient("Cenoura")
+    ingredient3 = Ingredient("Brócolis")
+    ingredient = Ingredient("Cenoura")
+    assert repr(ingredient) == "Ingredient('Cenoura')"
+
+    # Testa se o método mágico __eq__ funciona como esperado
+    assert ingredient1 == ingredient2
+    assert ingredient1 != ingredient3
+
+    # Testa se o método mágico __hash__ funciona como esperado
+    ingredient3 = Ingredient("Brócolis")
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
